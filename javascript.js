b5253e379e47cac41da2d73609e5c2f8dd348f23
micahart <31050596+micahart@users.noreply.github.com>
2018-02-13 16:58:03 -0700

    changed everything to grid method thing

--- javascript.js
+++ javascript.js
@@ -121,51 +121,104 @@ divtoshow.addEventListener("click", function () {
 
 var tics = document.getElementsByClassName("tic")
 
+var grid = [
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', '']
+];
+
+
+
+var winningCoordinates = [
+  //TODO: FIX COORDINATES TO BE ROW, THEN COLUMN
+    [[0,0],[0,1],[0,2]], //left column
+    [[1,0],[1,1],[1,2]], //middle column
+    [[2,0],[2,1],[2,2]], //right column
+    [[0,0],[1,0],[2,0]], // top row
+    [[0,1],[1,1],[2,1]], //middle row
+    [[0,2],[1,2],[2,2]], //bottom row
+    [[0,0],[1,1],[2,2]], //diagonal down to right
+    [[0,2],[1,1],[2,1]]  //diagonal up to right
+];
+
 function checkIfWon(){
-  console.log('cat')
-}
-
-for (var i = 0; i < tics.length; i = i + 1) {
-  var tic = tics[i];
-  tic.addEventListener("click", function() {
-    clickNumber = clickNumber + 1;
-
-// 0 % 2 = 0
-// 1 % 2 = 1
-// 2 % 2 = 0
-// 3 % 2 = 1
-// 4 % 2 = 0
-
-    // var grid = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-    // var grid = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-    // to access 1, you would do grid[0][0]
-    // to access 8, you would do grid[2][1]
-    // to access 9, you would do grid[2][2]
-
-   var grid = [
-     ['', '', ''],
-     ['', '', ''],
-     ['', '', '']
-   ];
-
-   if (this.innerHTML != '') {
-      console.log(this.innerHTML);
-      return;
-    }
-
-    // click logic
-    if (clickNumber % 2 === 1) {
-      // click number is odd
-      this.innerHTML = 'O';
-    } else {
-      // click number is even
-      this.innerHTML = 'X';
-    }
-
-    checkIfWon()
-
-  });
+  console.log('TODO: CHECK IF WON');
 }
 
 var clickNumber = 0;
+
+for (var i = 0; i < tics.length; i = i + 1) {
+
+  var tic = tics[i];
+  tic.addEventListener("click", function() {
+
+    // when someone clicks a tic:
+    //   find out the address in "grid" for that tic
+    //   modify the value of that address in "grid" (or not, if invalid move)
+    //   set the innerHtml of tic to that value
+
+    // var grid = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+    // 1:[0][0] (1-1)%3 = 0
+    // 2:[0][1]
+    // 3:[0][2]
+    // 4:[1][0]
+    // 6:[1][2]
+    // 7:[2][0]  (7-1)%3 = 0
+    // 8:[2][1] 8%3 = 1
+    // 9:[2][2]  9%3 = 2
+
+    // given id, find the address
+    // 1 % 3 = 1
+    // 2 % 3 = 2
+    // 3 % 3 = 0
+    // 4 % 3 = 1
+    // 5 % 3 = 2
+    // 6 % 3 = 0
+    // 7 % 3 = 1
+    // 8 % 3 = 2
+    // 9 % 3 = 0
+    // it gives you the remander
+
+    var id = this.id
+    var spotNumber =   parseInt(id);
+    console.log('id'+ id, 'spotNumber '+spotNumber);
+
+    // ID TO ROW
+    // 1,2,3 => 0
+    // 4,5,6 => 1
+    // 7,8,9 => 2
+
+    // ID TO COLUMN
+    // 1,4,7 => 0
+    // 2,5,8 => 1
+    // 3,6,9 => 2
+
+    var row = Math.floor((spotNumber-1) / 3); //Note: Math.floor() rounds down
+    var col = Math.floor((spotNumber+2) % 3 );
+
+    console.log('id',id,'is row',row,'col',col);
+    console.log('new grid:',grid);
+
+    if (grid[row][col] === '') {
+        //we can move
+
+        var move = '';
+
+        if (clickNumber % 2 === 1) {
+          // click number is odd
+          move = 'O';
+        } else {
+          // click number is even
+          move = 'X';
+        }
+
+        grid[row][col] = move;
+        this.innerHTML = move;
+
+        clickNumber = clickNumber + 1;
+
+        checkIfWon()
+    }
+
+  });
+}
